e7b14545f96ab331b65388793a49bfa4fa4a1528
Artem Kamenev(Jodzik) <novble@mail.ru>
2023-01-22 02:40:17 +0700

    Добавьте файлы проекта.

---
+++ firmware_utils.cpp
@@ -0,0 +1,12 @@
+﻿// firmware_utils.cpp: определяет точку входа для приложения.
+//
+
+#include "firmware_utils.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
